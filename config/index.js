6752eac60c7f17ff5b05b946f46c0ecda40cf25a
Chad Moravec <moravec@colorado.edu>
2021-03-08 15:11:42 -0700

    removed credentials

--- config/index.js
+++ config/index.js
@@ -1,13 +1,24 @@
-const Cloud = require('@google-cloud/storage')
-const path = require('path')
+const Cloud = require("@google-cloud/storage");
+const path = require("path");
 
-const serviceKey = path.join(__dirname, './keys.json')
+const serviceKey = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+};
 
-const { Storage } = Cloud
+const { Storage } = Cloud;
 
 const storage = new Storage({
   keyFilename: serviceKey,
-  projectId: "the-eyes-300618"
+  projectId: "the-eyes-300618",
 });
 
-module.exports = storage
+module.exports = storage;
